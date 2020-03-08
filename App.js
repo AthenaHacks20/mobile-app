0519ee1856cddc88324e4ba42b4ce13a0762c1ec
Diane Tobit <dianetobit@Dianes-MacBook-Pro.local>
2020-03-07 19:22:29 -0800

    setup signin screen

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, TextInput, Button } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AsyncStorage } from 'react-native';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
+import SignInScreen from './screens/SignInScreen';
 
 const Stack = createStackNavigator();
+const AuthContext = React.createContext();
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
   const [initialNavigationState, setInitialNavigationState] = React.useState();
   const containerRef = React.useRef();
   const { getInitialState } = useLinking(containerRef);
+
+  const [state, dispatch] = React.useReducer( (prevState, action) => {
+    if(action.type === 'RESTORE_TOKEN'){
+      return { 
+        ...prevState,
+        userToken:action.token,
+        isLoading:false
+      }
+    } 
+    else if (action.type === 'SIGN_IN'){
+      return { 
+        ...prevState,
+        isSignout: false,
+        userToken: action.token,
+      }
+    }
+    else if(action.type === 'SIGN_OUT'){
+      return { 
+        ...prevState,
+        isSignout: true,
+        userToken: null,
+      }
+    }},
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
 
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
@@ -40,24 +72,69 @@ export default function App(props) {
       }
     }
 
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
     loadResourcesAndDataAsync();
   }, []);
+
+  const authContext = React.createContext(
+    {
+      signIn: async data => {
+        //dummy token used rather than token from some server :(
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }
+  );
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return null;
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
+      <AuthContext.Provider value={authContext}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+              <Stack.Navigator>
+                { state.userToken == null ? (
+                  <Stack.Screen
+                    name="SignIn"
+                    component={SignInScreen}
+                    options={{
+                      title: 'Sign in',
+                      // When logging out, a pop animation feels intuitive
+                      // You can remove this if you want the default 'push' animation
+                      animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+                    }}
+                  />
+                ): (
+                  <Stack.Screen name="Root" component={BottomTabNavigator} />
+                )}
+              </Stack.Navigator>
+          </NavigationContainer>
+        </View>
+      </AuthContext.Provider>
     );
   }
 }
+
+export { AuthContext }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/SignInScreen.js
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import { Image, Platform, StyleSheet, TextInput, TouchableOpacity, View, Button} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser';
+import { AuthContext } from '../App';
+
+import { MonoText } from '../components/StyledText';
+
+export default function SignInScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const { signIn } = React.useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+            <TextInput
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            <Button title="Sign in" onPress={() => signIn({ username, password })} />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+// SignInScreen.navigationOptions = {
+//   header: null,
+// };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
