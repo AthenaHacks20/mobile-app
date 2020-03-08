c25476f9a078e18bd690e7d7203cb8fdf577cdfa
Nadya Djojosantoso <hello@nadyafebi.com>
2020-03-07 21:49:45 -0800

    Add map to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
+import { WebView } from 'react-native-webview';
 
 import { MonoText } from '../components/StyledText';
 
@@ -10,34 +11,13 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
+          <WebView
+            source={{ uri: 'http://athenahacks20-server.azurewebsites.net/map' }}
+            style={styles.mapContainer}
+            scalesPageToFit={false}
+            automaticallyAdjustContentInsets={false}
+          >
+          </WebView>
         </View>
       </ScrollView>
 
@@ -102,12 +82,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   contentContainer: {
-    paddingTop: 30,
+    flex: 1,
   },
   welcomeContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    flex: 1
   },
   welcomeImage: {
     width: 100,
@@ -176,4 +155,10 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
+  mapContainer: {
+    flex: 1,
+    height: 500,
+    width: 420,
+    resizeMode: 'cover'
+  }
 });
